0b08a0381033067d3671f67d7b92c618b85124d4
Sebastian Daniel <daniel@wobster.de>
2014-01-31 19:49:28 +0100

    german language added

---
+++ languages/de.php
@@ -0,0 +1,51 @@
+<?php
+
+add_translation('de', array(
+	
+
+	'annotation:group_topic_post:value:placeholder' => 'Antworten...',
+	'annotation:generic_comment:value:placeholder' => 'Kommentieren...',
+	
+	'composer:object:thewire' => "Status",
+	'composer:object:bookmarks' => "Lesezeichen",
+	'composer:object:blog' => "Blog",
+	'composer:annotation:messageboard' => "Post",
+	'composer:object:file' => 'Datei',
+	'composer:prompt' => 'Teilen',
+
+	'file:river:create' => 'hat eine Datei hochgeladen',
+	'files' => 'Dateien',
+	
+	'friend:user:add' => "%s als Freund hinzufügen",
+	
+	'groups:add' => 'Gruppe erstellen...',
+	
+	'home' => 'Startseite',
+	
+	'newsfeed' => 'novitá',
+	
+	'profile:wall' => "Pinnwand",
+	'profile:info' => "Info",
+	'profile:activity:none' => "No activity to display",
+	
+	'likes:likethis' => 'Like',
+	'likes:remove' => 'Unlike',
+	
+	'notifications:personal' => "Benachrichtigungen",
+	
+	'pages:river:create' => 'hat eine Seite erstellt',
+
+	'river:comment:object:thewire' => '',
+	'river:comments:all' => 'Alle %d Kommentare anzeigen',
+	'river:messageboard:group:default' => '',
+	'river:replies:all' => 'Alle %d Antworten anzeigen',
+	'river:to' => '%s &#x25B6 %s',
+	
+	'rss:subscribe' => 'Subscribe via RSS',
+
+	'see:all' => 'Alle anzeigen',
+	
+	'settings:user' => 'Kontoeinstellungen',
+
+
+));
